23d681cb7b69cabb6bb1cbceec0fe99d3e4746c0
Sarah Cronk <scronk@caci.co.uk>
2022-12-01 11:05:19 +0000

    Simplify day 1

--- 2022/Day01/day_01.py
+++ 2022/Day01/day_01.py
@@ -1,18 +1,23 @@
 input_data = open("2022/Day01/input.txt", "r", encoding="utf-8").read()
 sample_data = open("2022/Day01/sample.txt", "r", encoding="utf-8").read()
 
-def run(data, answer, answer_2):
-    list_form = data.split("\n\n")
-    sums = []
-    for item in list_form:
-        elf = [int(item) for item in item.split("\n")]
-        sums.append(sum(elf))
-
+def run_1(data, answer):
+    sums = get_sums(data)
     assert max(sums) == answer
 
+
+def run_2(data, answer):
+    sums = get_sums(data)
     sums.sort(reverse=True)
-    assert sum(sums[0:3]) == answer_2
+    assert sum(sums[0:3]) == answer
 
 
-run(sample_data, 24000, 45000)
-run(input_data, 64929, 193697)
+def get_sums(data):
+    return [sum([int(cal) for cal in item.split("\n")]) for item in data.split("\n\n")]
+
+
+run_1(sample_data, 24000)
+run_2(sample_data, 45000)
+
+run_1(input_data, 64929)
+run_2(input_data, 193697)
