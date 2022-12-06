9f9e84dd804d385e8dc6611d2ee111cc102cd12f
Sarah Cronk <scronk@caci.co.uk>
2022-12-06 10:24:13 +0000

    Day6

--- 2022/Day06/day_06.py
+++ 2022/Day06/day_06.py
@@ -2,12 +2,12 @@ input_data = open("2022/Day06/input.txt", "r", encoding="utf-8").read()
 sample_data = open("2022/Day06/sample.txt", "r", encoding="utf-8").read()
 
 def run(data):
-    for index, _char in enumerate(list(data)):
+    for index in range(0, len(data)):
         chars = set(data[index: index + 4])
         if len(chars) == 4:
             print(index + 4)
             break
-    for index, _char in enumerate(list(data)):
+    for index in range(0, len(data)):
         two_chars = set(data[index: index + 14])
         if len(two_chars) == 14:
             print(index + 14)
